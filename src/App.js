d6738e3337fa8962fcc7e9b99c6f4f971234649f
vinayak2882 <vinayak.patrut@gmail.com>
2022-05-29 13:54:45 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
+import { useState } from 'react';
+import { Grid, GridItem } from '@chakra-ui/react'
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+  
+<Grid
+  h='200px'
+  templateRows={['repeat(3, 1fr)','repeat(3, 1fr)']}
+  templateColumns={['repeat(1, 1fr)','repeat(3, 1fr)']}
+  gap={6}
+>
+  <GridItem colSpan={2} rowSpan={2} bg='mediumpurple' />
+  <GridItem rowSpan={3} colSpan={1} bg='tomato' />
+  <GridItem colSpan={2} bg='skyblue' />
+</Grid>
+
+<br></br>
+<br></br>
+<br></br>
+
+<Grid
+gridTemplateRows='repeat(4, 1fr)'
+ templateColumns='repeat(4, 1fr)' gap={0}>
+  <GridItem colSpan={1} h='10' bg='darkcyan' />
+  <GridItem colSpan={2} h='10' bg='darkorange' />
+  <GridItem colSpan={1} h='10' bg='crimson' />
+  {/* <GridItem colStart={4} colEnd={6} h='10' bg='papayawhip' /> */}
+</Grid>
+
+    </ChakraProvider>
   );
 }
 
